b50f0094aff95fbc7a38ddca7d0ce9891c9d38b5
evilj0ker <rajatsri94@gmail.com>
2020-09-13 03:03:45 +0530

    add lib/appRegistration

--- util/constants.js
+++ util/constants.js
@@ -5,6 +5,13 @@ const constants = {
 		staging: 'STAGING',
 		production: 'PRODUCTION'
 	},
+	contactName: '%CONTACT_NAME',
+	phoneNo: '%PHONE_NO',
 };
+
+constants.notificationMessage = {
+	title: `Contact ${constants.contactName} now`,
+	body: `It's time to contact ${constants.contactName}. Try calling on ${constants.phoneNo}`,
+},
 
 module.exports = constants;
