48ed4325e661e3257eb383e2b916ab7f4f0be22f
evilj0ker <rajatsri94@gmail.com>
2020-09-13 03:02:40 +0530

    use dotenv

--- config/production/db.js
+++ config/production/db.js
@@ -1,5 +1,5 @@
-const mongoUser = 'phoenix';
-const mongoPassword = '6YvjRq2ypX6jHHca';
+const mongoUser = process.env.MONGO_USER;
+const mongoPassword = process.env.MONGO_PASSWORD;
 
 module.exports = {
 	mongoServer: `mongodb+srv://${mongoUser}:${mongoPassword}@geekparadox.mmr1h.gcp.mongodb.net/keepintouch?retryWrites=true&w=majority`
